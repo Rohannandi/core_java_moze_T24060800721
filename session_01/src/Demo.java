75bbc2a90e42e6b6d936c6f7f94ad3773d5e30ca
Priyanka <priyankanandi25@gmail.com>
2024-03-07 11:28:16 +0530

    Final Commit

---
+++ session_01/src/Demo.java
@@ -0,0 +1,25 @@
+
+public class Demo {
+	static int a;
+	static boolean b;
+	static short c;
+	static char d;
+	static float e;
+	static double f;
+	static long g;
+	
+	
+	
+	
+	
+	public static void main(String args[])
+	{
+		System.out.println("Default value of int="+a);
+		System.out.println("Default value of boolean="+b);
+		System.out.println("Default value of short="+c);
+		System.out.println("Default value of char="+d);
+		System.out.println("Default value of float="+e);
+		System.out.println("Default value of double="+f);
+		System.out.println("Default value of long="+g);
+	}
+}
